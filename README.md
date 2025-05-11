f25b8b85677d4d477b74db5c444109e9b56abf58
Carmen Hoyt <hoyt.carmen@gmail.com>
2025-05-11 11:03:01 -0700

    Update all.

---
+++ README.md
@@ -0,0 +1,55 @@
+# Moorea LTER SQL Analysis
+
+### Authors
+
+Carmen Hoyt [@ceh58](https://github.com/ceh58)
+
+## About 
+
+**"How does benthic water temperature relate to crown of thorn abundance?"**
+
+This repository hosts the code for creating a SQL database for Moorea LTER data, specifically benthic water temperature and Acanthaster planci abundance counts, and the resulting analysis. 
+
+## Data
+
+Data was downloaded as .csv files from the [Moorea LTER catalog](https://mcr.lternet.edu/data).
+
+Two time series were selected:
+
+1. [Benthic water temperature](https://portal.edirepository.org/nis/metadataviewer?packageid=knb-lter-mcr.1035.16):
+  - 2005-2023
+  - temp (C)
+  - sites 1-6 (each as individual .csv files)
+
+2. [Population Dynamics of Acanthaster planci (crown of thorns starfish)](https://portal.edirepository.org/nis/metadataviewer?packageid=knb-lter-mcr.1039.11):
+  - 2005-2022
+  - counts
+  - sites 1-6 (one .csv file)
+
+Data was housed in a moorea-LTER-data/ folder, cleaned in R, and loaded in to moorea_LTER.db using DuckDB. Visualizations were produced using {ggplot}, a part of the {tidyverse},in R.
+
+## Repository Structure
+
+```
+├── .gitignore
+├── moorea_LTER-data/ # data not pushed to github due to size
+|    ├── benthic-water-temp/
+|    |    ├── MCR_LTER01_BottomMountThermistors_20230323.csv
+|    |    ├── MCR_LTER02_BottomMountThermistors_20230323.csv
+|    |    ├── MCR_LTER03_BottomMountThermistors_20230323.csv
+|    |    ├── MCR_LTER04_BottomMountThermistors_20230323.csv
+|    |    ├── MCR_LTER05_BottomMountThermistors_20230323.csv
+|    |    └── MCR_LTER06_BottomMountThermistors_20230323.csv
+|    ├── MCR_LTER_COTS_abundance_2005-2022_20220503.csv
+|    ├── ac_abundance_cleaned.csv
+|    └── benthic_water_temp_cleaned.csv
+├── moorea_LTER.db # database not pushed to github due to size
+|    ├── Abundance
+|    ├── Water_temp
+|    └── Water_abundance_combined
+├── eds-213-discussion.Rproj
+├── moorea-LTER.qmd
+└── moorea_LTER.sql
+```
+
+<img src="starfish.png" alt="starfish" style="width:50%;">
---
+++ moorea_LTER.sql
@@ -0,0 +1,172 @@
+-- Create Database
+
+.open moorea-LTER.db
+
+-- Reset tables
+DROP TABLE Abundance;
+DROP TABLE Water_temp;
+
+-- Create Abundance table
+CREATE TABLE Abundance (
+  year INT NOT NULL,
+  site VARCHAR NOT NULL,
+  habitat VARCHAR NOT NULL,
+  transect INT NOT NULL,
+  UNIQUE (year, site, habitat, transect),
+  a_planci_counts INT NOT NULL
+);
+
+COPY Abundance FROM 'clean-data/ac_abundance_cleaned.csv' (header TRUE);
+
+-- Create Water_temp table
+CREATE TABLE Water_temp (
+  site VARCHAR NOT NULL,
+  time_local DATE NOT NULL,
+  reef_type_code VARCHAR NOT NULL,
+  sensor_type VARCHAR NOT NULL,
+  sensor_depth_m INT NOT NULL,
+  temperature_c REAL NOT NULL,
+  year INT NOT NULL
+);
+
+COPY Water_temp FROM 'clean-data/benthic_water_temp_cleaned.csv' (header TRUE);
+
+-- Look at data
+SELECT * FROM Abundance;
+SELECT * FROM Water_temp;
+
+-- Answer questions
+--- How does benthic water temperature relate to crown of thorn abundance? By habitat type?
+
+CREATE TABLE final AS (
+  SELECT 
+  at.site, 
+  AVG(at.avg_temp) AS avg_temp, 
+  SUM(sa.abundance) AS abundance
+FROM (
+  SELECT site, year, AVG(temperature_c) AS avg_temp
+  FROM Water_temp
+  GROUP BY site, year
+) at
+JOIN (
+  SELECT site, year, SUM(a_planci_counts) AS abundance
+  FROM Abundance
+  GROUP BY site, year
+) sa USING (site, year)
+GROUP BY at.site
+);
+
+SELECT * FROM final;
+
+SELECT * FROM abundance;
+
+---------------------------------------------------------
+
+CREATE TABLE final_habitat AS (
+  SELECT 
+    at.site, 
+    sa.habitat,
+    AVG(at.avg_temp) AS avg_temp, 
+    SUM(sa.abundance) AS abundance
+  FROM (
+    SELECT site, year, AVG(temperature_c) AS avg_temp
+    FROM Water_temp
+    GROUP BY site, year
+  ) at
+  JOIN (
+    SELECT site, year, habitat, SUM(a_planci_counts) AS abundance
+    FROM Abundance
+    GROUP BY site, year, habitat
+  ) sa USING (site, year)
+  GROUP BY at.site, sa.habitat
+);
+
+SELECT * FROM final_habitat;
+
+---------------------------------------------------------
+
+
+-- Sum abundance by year and site
+CREATE TABLE sum_abundance AS (
+  SELECT year, site, SUM(a_planci_counts) AS abundance
+  FROM Abundance
+  GROUP BY year, site
+);
+
+-- Average water temp by year and site
+CREATE TABLE avg_temp AS (
+  SELECT year, site, AVG(temperature_c) AS avg_temp
+  FROM Water_temp
+  GROUP BY year, site
+);
+
+CREATE TABLE temp_abundance AS (
+  SELECT site, year, avg_temp, abundance 
+  FROM avg_temp 
+  JOIN sum_abundance USING (site, year)
+  GROUP BY site, year, avg_temp, abundance
+);
+
+-- Group by site over all years
+SELECT site, AVG(avg_temp) AS avg_temp, SUM(abundance) AS abundance
+  FROM temp_abundance
+  GROUP BY site;
+
+-- Look at LTER01 site over all years
+SELECT site, year, avg_temp, SUM(abundance) AS abundance
+FROM temp_abundance
+WHERE site = 'LTER01'
+GROUP BY site, year, avg_temp
+ORDER BY year DESC;
+
+CREATE TABLE Final_table AS(
+SELECT site, year, avg_temp, SUM(abundance) AS abundance
+FROM temp_abundance
+GROUP BY site, year, avg_temp
+ORDER BY year DESC
+);
+
+-- by habitat type
+--- Sum abundance by year and habitat
+CREATE TEMP TABLE sum_abundance_habitat AS (
+  SELECT year, habitat, SUM(a_planci_counts) AS abundance
+  FROM Abundance
+  GROUP BY year, habitat
+);
+
+SELECT * FROM sum_abundance_habitat;
+
+-- average water temp by year and habitat
+CREATE TEMP TABLE avg_temp_habitat AS (
+  SELECT year, reef_type_code, AVG(temperature_c) AS avg_temp
+  FROM Water_temp
+  GROUP BY year, reef_type_code
+);
+
+-------------------------------------------------------
+
+CREATE TABLE temp_abundance AS (
+  SELECT site, year, avg_temp, abundance 
+  FROM avg_temp 
+  JOIN sum_abundance USING (site, year)
+  GROUP BY site, year, avg_temp, abundance
+);
+
+-- group by site over all years
+SELECT site, AVG(avg_temp) AS avg_temp, SUM(abundance) AS abundance
+  FROM temp_abundance
+  GROUP BY site;
+
+-- look at LTER01 site over all years
+SELECT site, year, avg_temp, SUM(abundance) AS abundance
+FROM temp_abundance
+WHERE site = 'LTER01'
+GROUP BY site, year, avg_temp
+ORDER BY year DESC;
+
+CREATE TABLE Final_table AS(
+SELECT site, year, avg_temp, SUM(abundance) AS abundance
+FROM temp_abundance
+GROUP BY site, year, avg_temp
+ORDER BY year DESC
+);
